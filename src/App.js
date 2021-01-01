67bcff6321e790611f99045c8e67e093e1427057
ayushrana182 <ayushrana182@gmail.com>
2021-01-01 09:42:20 +0545

    CSS in JS with Emotion Library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
+import styled from '@emotion/styled';
 
 const PokemonRow = ({ pokemon, onSelect }) => (
   <tr>
@@ -52,6 +53,30 @@ PokemonInfo.propTypes = {
   }),
 };
 
+//CSS in JS
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const TwoColumnLayout = styled.div`
+  display: 'grid';
+  grid-template-columns: '70% 30%';
+  grid-column-gap: '1rem';
+`;
+
+const Container = styled.div`
+  margin: 'auto';
+  width: 800px;
+  padding-top: '1rem';
+`;
+
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+}
+`;
+
 function App() {
   const [filter, filterSet] = React.useState('');
   const [pokemon, pokemonSet] = React.useState([]);
@@ -64,22 +89,10 @@ function App() {
   }, []);
 
   return (
-    <div
-      style={{
-        margin: 'auto',
-        width: 800,
-        paddingTop: '1rem',
-      }}
-    >
-      <h1 className='title'>Pokemon Search</h1>
-      <input value={filter} onChange={(evt) => filterSet(evt.target.value)} />
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '70% 30%',
-          gridColumnGap: '1rem',
-        }}
-      >
+    <Container>
+      <Title>Pokemon Search</Title>
+      <Input value={filter} onChange={(evt) => filterSet(evt.target.value)} />
+      <TwoColumnLayout>
         <div>
           <table width='100%'>
             <thead>
@@ -111,8 +124,8 @@ function App() {
             <h1>{selectedItem && <PokemonInfo {...selectedItem} />}</h1>
           </div>
         )}
-      </div>
-    </div>
+      </TwoColumnLayout>
+    </Container>
   );
 }
 
